97da9ba1560cee4fbd45740ddebf0ef780199ec6
thuanbua562004 <157040478+thuanbua562004@users.noreply.github.com>
2024-07-04 14:48:53 +0700

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/cuoiki/HomeActivity.java
+++ app/src/main/java/com/example/cuoiki/HomeActivity.java
@@ -1,79 +1,79 @@
 
-package com.example.cuoiki;
+    package com.example.cuoiki;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+    import android.content.Context;
+    import android.content.Intent;
+    import android.content.SharedPreferences;
+    import android.os.Bundle;
+    import androidx.appcompat.app.AppCompatActivity;
 
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageButton;
+    import android.util.Log;
+    import android.view.View;
+    import android.widget.ImageButton;
 
-public class HomeActivity extends AppCompatActivity {
-    ImageButton   news,user,home ,menu , chinhsua , lichhoc ,yeucau , dangxuat;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
-        anhxa();
-        menubar();
-        SharedPreferences sharedPref = getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
-        String mssv = sharedPref.getString("key", "");
-        Log.e("TAG1", "onCreate: " + mssv  );
+    public class HomeActivity extends AppCompatActivity {
+        ImageButton   news,user,home ,menu , chinhsua , lichhoc ,yeucau , dangxuat;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.home);
+            anhxa();
+            menubar();
+            SharedPreferences sharedPref = getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
+            String mssv = sharedPref.getString("key", "");
+            Log.e("TAG1", "onCreate: " + mssv  );
+        }
+        public void menubar(){
+            news.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(HomeActivity.this, NotificationActivity.class);
+                    startActivity(intent);
+                }
+            });
+            user.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(HomeActivity.this , UserActivity.class));
+                }
+            });
+            menu.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(HomeActivity.this , MenuUser.class));
+
+                }
+            });
+            chinhsua.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity( new Intent(HomeActivity.this , UserUpdate.class));
+                }
+            });
+            lichhoc.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity( new Intent(HomeActivity.this , LichHocActivity.class));
+                }
+            });
+            dangxuat.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    SharedPreferences sharedPref = getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.clear();
+                    editor.apply();
+                    startActivity(new Intent(HomeActivity.this , MainActivity.class));            }
+            });
+
+        }
+        public void anhxa(){
+            user = findViewById(R.id.user);
+            home = findViewById(R.id.home);
+            news  =findViewById(R.id.news);
+            menu = findViewById(R.id.menu);
+            chinhsua = findViewById(R.id.chinhsua);
+            lichhoc = findViewById(R.id.lichhoc);
+            dangxuat = findViewById(R.id.dangxuat);
+        }
     }
-    public void menubar(){
-        news.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, NotificationActivity.class);
-                startActivity(intent);
-            }
-        });
-        user.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(HomeActivity.this , UserActivity.class));
-            }
-        });
-        menu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(HomeActivity.this , MenuUser.class));
-
-            }
-        });
-        chinhsua.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity( new Intent(HomeActivity.this , UserUpdate.class));
-            }
-        });
-        lichhoc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity( new Intent(HomeActivity.this , LichHocActivity.class));
-            }
-        });
-        dangxuat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SharedPreferences sharedPref = getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPref.edit();
-                editor.clear();
-                editor.apply();
-                startActivity(new Intent(HomeActivity.this , MainActivity.class));            }
-        });
-
-    }
-    public void anhxa(){
-        user = findViewById(R.id.user);
-        home = findViewById(R.id.home);
-        news  =findViewById(R.id.news);
-        menu = findViewById(R.id.menu);
-        chinhsua = findViewById(R.id.chinhsua);
-        lichhoc = findViewById(R.id.lichhoc);
-        dangxuat = findViewById(R.id.dangxuat);
-    }
-}
